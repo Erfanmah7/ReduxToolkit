8daa233098cc5a0f5e95518304b57d14a603af66
Erfanmah7 <efn8mah@yahoo.com>
2024-07-09 23:21:24 +0330

    extraReducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "../features/counter/counterSlice";
 import numberSlice from "../features/number/numberSlice";
+import logger from "react-logger";
 
 const counterStore = configureStore({
   reducer: {
     counter: counterSlice,
     number: numberSlice,
   },
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
 
 export default counterStore;
